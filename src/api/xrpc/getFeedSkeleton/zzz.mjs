ca0731a5618018d3a93cf6bea18f7a10ab602b95
kdanni <kdanni@kdanni.hu>
2025-07-08 01:56:31 +0200

    log fix

--- src/api/xrpc/getFeedSkeleton/zzz.mjs
+++ src/api/xrpc/getFeedSkeleton/zzz.mjs
@@ -1,5 +1,9 @@
+const DEV_ENV = process.env.ENV === 'DEV';
+const DEBUG = process.env.DEBUG === 'true' || false;
+
 async function handleRequest (req, res, next) {
-    console.log('[getFeedSkeleton/zzz]');    
+    DEBUG && console.log('[getFeedSkeleton/zzz]', 'Request.locals:', req.locals, 'Response.locals:', res.locals);
+    DEV_ENV && !DEBUG && console.log('[getFeedSkeleton/zzz]', req.locals.cacheKey, 'FeedData:', `${res.locals.feedData}`, 'CachedData:', `${res.locals.cachedData}`);
     if(!res.locals.feedData) {
         res.json({
             feed: [],

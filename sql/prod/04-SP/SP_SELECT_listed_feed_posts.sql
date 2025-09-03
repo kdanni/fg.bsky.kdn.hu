7f235f2e4cb26700de3bfe7c389087587f5dbacd
kdanni <kdanni@kdanni.hu>
2025-09-03 11:26:43 +0200

    🏳️‍🌈

--- sql/prod/04-SP/SP_SELECT_listed_feed_posts.sql
+++ sql/prod/04-SP/SP_SELECT_listed_feed_posts.sql
@@ -24,7 +24,7 @@ BEGIN
     SELECT *
     FROM listed_post
     WHERE posted_at < cursor_date AND list_name <> 'NSFW' AND list_name <> 'Not Listed' 
-        AND list_name <> 'My Followers' AND list_name <> "Let's see"
+        AND list_name <> 'My Followers' AND list_name <> "Let's see" AND '🏳️‍🌈'
     -- AND (has_image = 'image/' OR image_only = false OR image_only IS NULL)
     ORDER BY posted_at DESC
     LIMIT p_limit;
--- sql/prod/04-SP/SP_SELECT_listed_feed_posts_my_followes.sql
+++ sql/prod/04-SP/SP_SELECT_listed_feed_posts_my_followes.sql
@@ -24,7 +24,7 @@ BEGIN
     SELECT *
     FROM listed_post
     WHERE posted_at < cursor_date 
-        AND (list_name = 'My Followers' OR list_name = "Let's see")
+        AND (list_name = 'My Followers' OR list_name = "Let's see" OR list_name = '🏳️‍🌈')
     -- AND (has_image = 'image/' OR image_only = false OR image_only IS NULL)
     ORDER BY posted_at DESC
     LIMIT p_limit;

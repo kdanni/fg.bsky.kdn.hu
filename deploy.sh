01fe48bcf233c017de5678ed2470519246de9c50
kdanni <kdanni@kdanni.hu>
2025-09-12 18:31:32 +0200

    search paralel

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ git pull
 # npm run dbinstall
 
 sudo systemctl restart server.fg.bsky.service
-sudo systemctl restart backfill.fg.bsky.service
 sudo systemctl restart jetstream.fg.bsky.service
 
-sudo journalctl -qfu backfill.fg.bsky.service -u server.fg.bsky.service -u jetstream.fg.bsky.service
+sudo journalctl -qfu backfill.fg.bsky.service -u server.fg.bsky.service -u jetstream.fg.bsky.service -u actor-backfill.fg.bsky.service -u fav-backfill.fg.bsky.service
--- journal.sh
+++ journal.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo journalctl -qfu backfill.fg.bsky.service -u server.fg.bsky.service -u jetstream.fg.bsky.service
+sudo journalctl -qfu backfill.fg.bsky.service -u server.fg.bsky.service -u jetstream.fg.bsky.service -u actor-backfill.fg.bsky.service -u fav-backfill.fg.bsky.service

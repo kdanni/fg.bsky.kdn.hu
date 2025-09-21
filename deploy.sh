0311708f37f11aabead80020a037e26886399525
kdanni <kdanni@kdanni.hu>
2025-09-21 12:44:35 +0200

    -u js-bf-nsfw.fg.bsky.service

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ git pull
 sudo systemctl restart server.fg.bsky.service
 sudo systemctl restart jetstream.fg.bsky.service
 
-sudo journalctl -qfu backfill.fg.bsky.service -u server.fg.bsky.service -u jetstream.fg.bsky.service -u actor-backfill.fg.bsky.service -u fav-backfill.fg.bsky.service
+sudo journalctl -qfu backfill.fg.bsky.service -u server.fg.bsky.service -u jetstream.fg.bsky.service -u actor-backfill.fg.bsky.service -u fav-backfill.fg.bsky.service -u js-bf-nsfw.fg.bsky.service

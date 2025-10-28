44b9cec090a982a3206a7963d2db2762b206b457
kdanni <kdanni@kdanni.hu>
2025-10-28 21:56:35 +0100

    feed teaks

--- src/feed-config/feed-of-feeds/urbex-streetart.mjs
+++ src/feed-config/feed-of-feeds/urbex-streetart.mjs
@@ -1,10 +1,16 @@
 import { shortname as ub } from '../search-feed/urbex.mjs';
 import { shortname as gr } from '../search-feed/graffiti.mjs';
 
+import { shortname as sanpo } from '../search-feed/sanpoHash.mjs';
+import { shortname as rodjiura } from '../search-feed/rodjiuraHash.mjs';
+
+const JAP = '.streetphotographyJapan'; 
+
+
 const NOT_PUBLISHED_FEED_NAME = '.urbexFeedOfFeeds'; 
 
 export const shortname = 'urbex-streetart';
-export const shortnameArray = [ub, gr, NOT_PUBLISHED_FEED_NAME];
+export const shortnameArray = [ub, gr, sanpo, rodjiura, JAP, NOT_PUBLISHED_FEED_NAME];
 
 export const FEEDGEN_CONFIG = {
   publisherDid: `${process.env.FEEDGEN_PUBLISHER_DID}`,
